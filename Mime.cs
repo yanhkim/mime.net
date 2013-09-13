fc3318981d54ebb6bbd68aa8bb3861308bc2c483
Namhoon Kim <yanhkim@gmail.com>
2013-09-13 14:49:48 +0900

    initial commit

---
+++ Mime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mime.net
+{
+    public static class Mime
+    {
+        public static string Lookup(string path) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+mime.net
+========
+
+.NET port of node.js module [mime][1].
+
+
+Target framework
+----------------
+
+.NET Framework 3.0
+
+
+[1]: https://github.com/broofa/node-mime
